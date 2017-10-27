a55fb8b986cd3e28dfbdcd4ece0e0ed075ad801d
taehoonkong <dahlia0111@gmail.com>
2017-10-27 17:11:00 +0900

    hostname change

--- api/src/router/auth.js
+++ api/src/router/auth.js
@@ -279,7 +279,7 @@ module.exports = function(io) {
                   subject: 'Encore Password Reset',
                   text: 'You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n' +
                         'Please click on the following link, or paste this into your browser to complete the process:\n\n' +
-                        'http://' + req.headers.host + '/auth/reset/' + token + '\n\n' +
+                        'https://' + 'encoreserver.texcore.io' + '/auth/reset/' + token + '\n\n' +
                         'If you did not request this, please ignore this email and your password will remain unchanged.\n'
                 }
                 sgMail.send(msg, (err) => {
